8c5f307793b81ae27c8411974cb901866ac44c2a
olavemil <olav_oee@msn.com>
2016-01-23 16:14:43 +0100

    Begun implementing delegateBehavior

---
+++ src/behavior/delegateBehavior.ts
@@ -0,0 +1,11 @@
+//delegateBehavior(insight, needs.Where(value > needs.max / 2).count) => 
+namespace Game {
+    export class delegateBehavior extends Behavior {
+        calcUrgency():number {
+            return this.agent.attributes.insight.getValue() * (this.agent.needs);
+        }
+        update():number {
+            
+        }
+    }
+}
